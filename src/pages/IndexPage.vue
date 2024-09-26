9bbce40af6c63c4bfe4b2f26e739130d86f72d2f
Sover <thesoverman@gmail.com>
2024-09-26 17:51:58 +0300

    service

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,15 +1,34 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
-  </q-page>
+  <div>
+    <h1>Weather Information</h1>
+    <p v-if="weather">City: {{ weather.name }}</p>
+    <p v-if="weather">Temperature: {{ weather.main.temp }}°C</p>
+    <p v-if="weather">Weather: {{ weather.weather[0].description }}</p>
+    <p v-if="error">{{ error }}</p>
+    <button @click="fetchWeather">Get Weather</button>
+  </div>
 </template>
 
-<script setup>
-defineOptions({
-  name: 'IndexPage'
-});
+<script>
+import { WeatherService } from "../weatherService";
+
+export default {
+  data() {
+    return {
+      weather: null,
+      error: null,
+    };
+  },
+  methods: {
+    async fetchWeather() {
+      try {
+        this.error = null;
+        const city = "Kyiv"; // Змініть на бажане місто
+        this.weather = await WeatherService.getWeatherByCity(city);
+      } catch (err) {
+        this.error = "Failed to fetch weather data";
+      }
+    },
+  },
+};
 </script>
