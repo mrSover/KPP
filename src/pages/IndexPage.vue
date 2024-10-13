afe97f80e5ba05c050d16ef5fb2dd8e4b5aa1d7c
JeFFlidan <zaveruha678@gmail.com>
2024-10-13 18:57:38 +0300

    Added icon to daily forecast buttons. Added temperature and humidity charts

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,85 +1,115 @@
 <template>
 
   <div :class="['weather-page', isDay ? 'day' : 'night']">
-    <!-- Поиск города -->
+    
     <div class="search-container">
       <q-input
         v-model="city"
-        label="Enter city"
         filled
+        placeholder="Enter city"
         @keydown.enter="fetchWeatherByCity"
-        input-style="color: rgba(234, 224, 243); font-size: 22px; text-align: center;"
+        input-style="color: rgba(234, 224, 243); font-size: 22px; text-align: left;"
       />
-
     </div>
-    <q-btn label="Get Weather"
-    style="color: rgba(244, 238, 244);  background-color: #162944; padding: 14px; border-radius: 25px; border-top-left-radius: 0px; margin-bottom: 20px; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.5);"
-    @click="fetchWeatherByCity" />
-    <!-- Информация о погоде -->
+
     <div v-if="weatherData" class="weather-info">
+
       <div class="header-container">
         <h2>{{ displayCity }}</h2>
         <img :src="getWeatherIcon(weatherData.weather[0].icon)" alt="Weather Icon" class="weather-icon" />
-      </div>
-      <div class="current-weather">
-        <div class="weather-box">
-          <p>Temperature: {{ weatherData.main.temp }} °C</p>
-          <p>Weather: {{ weatherData.weather[0].description }}</p>
-          <p>Humidity: {{ weatherData.main.humidity }}%</p>
+        <div class="current-weather-data">
+          Temperature: {{ weatherData.main.temp }} °C
+          <br>Humidity: {{ weatherData.main.humidity }}%
+          <br>Wind: {{ weatherData.wind.speed }} m/s
+        </div>
+        <div class="weather-desc">
+          Weather
+          <br>{{ getCurrentDayName() }}
+          <br>{{ capitalizeFirstLetter(weatherData.weather[0].description) }}
         </div>
       </div>
 
-      <!-- Прогноз на 10 часов -->
-      <div class="hourly-forecast">
-        <h3>Hourly Forecast</h3>
-        <div class="forecast-grid">
-          <div v-for="hour in hourlyForecast" :key="hour.time_epoch" class="hour-box">
-            <p>{{ formatTime(hour.time_epoch) }}: {{ hour.temp_c }} °C </p>
-          </div>
-        </div>
+      <div class="tabs">
+        <q-btn
+          :class="{ active: activeTab === 'temperature' }"
+          @click="fillChart('temperature')"
+          label="Temperature"
+          flat
+          unelevated
+        />
+        <q-btn
+          :class="{ active: activeTab === 'humidity' }"
+          @click="fillChart('humidity')"
+          label="Humidity"
+          flat
+          unelevated
+        />
       </div>
-      <hr>
-      <!-- Прогноз на 7 дней -->
+
+      <div class="chart-container">
+        <apexchart 
+          width="960px"
+          height="350px"
+          type="line" 
+          :options="chartOptions" 
+          :series="chartSeries">
+        </apexchart>
+      </div>
+
       <div class="daily-forecast">
         <h3>7-Day Forecast</h3>
-        <div class="forecast-grid">
-          <div v-for="day in dailyForecast" :key="day.date_epoch" class="day-box">
-            <p>{{ formatDate(day.date_epoch) }}: {{ day.day.avgtemp_c }} °C</p>
-          </div>
+        <div>
+          <q-btn
+            v-for="day in dailyForecast"
+            :key="day.date_epoch"
+            class="day-box"
+            @click="onDayClick(day)"
+            :label="null"
+            flat
+            unelevated
+            :style="{ color: 'rgb(224, 232, 243)' }"
+            v-html="getButtonLabel(day)"
+            :class="{ selected: selectedDay === day }"
+          />
         </div>
       </div>
     </div>
 
-    <!-- Сообщение об ошибке -->
+    <!-- Error message -->
     <div v-else-if="city && !weatherData" style="margin-top: 15px;">
       <p>No weather data available for {{ displayCity }}. Try again.</p>
     </div>
   </div>
-  <footer>
-        <p>&copy; 2024 All rights reserved</p>
-    </footer>
 </template>
 
 <script>
 
 import { WeatherService } from "../../service/WeatherService";
+import VueApexCharts from "vue3-apexcharts";
 
 export default {
   data() {
     return {
-      city: 'Dnipro', // Город по умолчанию
+      city: 'Dnipro', // Default city
       displayCity: 'Dnipro',
-      weatherData: null, // Данные о погоде
-      hourlyForecast: [], // Прогноз на следующие 10 часов
-      dailyForecast: [], // Прогноз на 7 дней
-      isDay: true, // Проверка дня или ночи
+      weatherData: null, // Info about current weather
+      dailyForecast: [], // Forecast for 7 days
+      isDay: true, // Day or night check
+      chartOptions: [],
+      chartSeries: [],
+      activeTab: 'temperature', // Default active tab
+      selectedDay: null,
+      twoHourIntervals: [],
     };
   },
+  components: {
+    apexchart: VueApexCharts
+  },
   methods: {
-    // Получение погоды по названию города
     getWeatherIcon(iconCode) {
-    return `http://openweathermap.org/img/wn/${iconCode}@2x.png`;
+      return `http://openweathermap.org/img/wn/${iconCode}@2x.png`;
     },
+
     async fetchWeatherByCity() {
       if (!this.city) {
         console.error('City name is required.');
@@ -88,34 +118,154 @@ export default {
 
       try {
         this.displayCity = this.city;
-        // Получаем данные о погоде (текущая погода от OpenWeather)
-        this.weatherData = await WeatherService.getWeatherByCity(this.city);
+        this.city = '';
+        this.weatherData = await WeatherService.getWeatherByCity(this.displayCity);
+        this.dailyForecast = await WeatherService.getForecastDay(this.displayCity, 7);
 
-        // Прогноз на 10 часов (от WeatherAPI)
-        this.hourlyForecast = await WeatherService.getForecastDayHour(this.city, 1);
-
-        // Прогноз на 7 дней (от WeatherAPI)
-        this.dailyForecast = await WeatherService.getForecastDay(this.city, 7);
-
-        // Определяем день или ночь
         const currentHour = new Date().getHours();
         this.isDay = currentHour >= 6 && currentHour < 18;
+        console.log(this.weatherData)
+        this.selectedDay = this.dailyForecast[0];
+        this.fillChart('temperature');
+        
       } catch (error) {
         console.error('Error fetching weather data:', error);
-        this.weatherData = null; // Сбрасываем данные при ошибке
+        this.weatherData = null;
       }
+
     },
+
     formatTime(timestamp) {
       const date = new Date(timestamp * 1000);
       return date.getHours() + ':00';
     },
+
     formatDate(timestamp) {
       const date = new Date(timestamp * 1000);
-      return date.toDateString();
+      const options = { weekday: 'short', month: 'short', day: 'numeric' };  // Customize formatting options
+      return date.toLocaleDateString(undefined, options);  // Return formatted date (e.g., "Oct 13")
     },
+
+    getCurrentDayName() {
+      const date = new Date();
+      const options = { weekday: 'long' }; // Use 'short' for abbreviated day names (e.g., Mon, Tue)
+      return date.toLocaleDateString(undefined, options);
+    },
+
+    getButtonLabel(day) {
+      return `
+        ${this.formatDate(day.date_epoch)}
+        <img src="${day.day.condition.icon}" alt="Weather Icon" class="weather-icon" />
+        ${day.day.maxtemp_c}° ${day.day.mintemp_c}°
+        `;
+    },
+
+    capitalizeFirstLetter(string) {
+      if (!string) return string;
+      return string.charAt(0).toUpperCase() + string.slice(1);
+    },
+
+    onDayClick(day) {
+      this.selectedDay = day;
+      this.fillChart(this.activeTab);
+    },
+
+    fillChart(activeTabName) {
+      if (activeTabName === 'temperature') {
+        this.fillTemperatureChart(this.selectedDay);
+      } else if (activeTabName === 'humidity') {
+        this.fillHumidityChart(this.selectedDay);
+      }
+
+      this.activeTab = activeTabName;
+    },
+
+    fillChartOptions(xAxisData) {
+      this.chartOptions = {
+        chart: {
+          type: 'line',
+          toolbar: {
+            show: false
+          },
+          zoom: {
+            enabled: false
+          }
+        },
+        
+        yaxis: {
+          labels: {
+            show: false
+          }
+        },
+        xaxis: {
+          categories: xAxisData.map(item => this.formatTime(item.time_epoch)),
+          labels: {
+            style: {
+              colors: 'white',
+            }
+          },
+        },
+        dataLabels: {
+          enabled: true
+        },
+        grid: {
+          show: false
+        },
+        dataLabels: {
+          enabled: true,
+          offsetY: -7,
+          background: {
+            enabled: false
+          },
+          style: {
+            colors: ['#e0e8f3']
+          }
+        }
+      };
+    },
+
+    fillTemperatureChart(day) {
+      const twoHourIntervals = day.hour.filter((item, index) => index % 2 === 0);
+
+      this.chartSeries = [{ name: 'Temperature', data: [] }];
+
+      for (let i = 0; i < day.hour.length; ++i) {
+        if (i % 2 === 0) {
+          this.chartSeries[0].data.push(day.hour[i].temp_c);
+        }
+      }
+
+      this.fillChartOptions(twoHourIntervals);
+
+      this.chartOptions.stroke = {
+        curve: 'smooth',
+        width: 2,
+        colors: ['#f8e255']
+      };
+    },
+
+    fillHumidityChart(day) {
+      const twoHourIntervals = day.hour.filter((item, index) => index % 2 === 0);
+
+      this.chartSeries = [{ name: 'Humidity', data: [] }];
+
+      for (let i = 0; i < day.hour.length; ++i) {
+        if (i % 2 === 0) {
+          this.chartSeries[0].data.push(day.hour[i].humidity);
+        }
+      }
+
+      this.fillChartOptions(twoHourIntervals);
+
+      this.chartOptions.stroke = {
+        curve: 'smooth',
+        width: 2,
+        colors: ['#3ca1e2']
+      }
+    }
   },
+
   mounted() {
-    // Загружаем погоду для города по умолчанию при загрузке компонента
     this.fetchWeatherByCity();
   },
 };
@@ -123,22 +273,26 @@ export default {
 
 <style scoped>
 .header-container {
+  width: 960px;
   display: flex;
-  align-items: center; /* Вертикальное выравнивание по центру */
-  justify-content: center;
-  padding-left: 20px;
+  align-items: center;
+  justify-content: flex-start;
+  margin: 0 auto;
 }
 .weather-icon {
-  width: 50px; /* размер иконки */
-  height: 50px;
-  margin-right: 5px; /* пространство между иконкой и текстом */
+  width: 100px;
+  height: 100px;
+  margin-right: 5px;
 }
 .weather-page {
   max-width: 100%;
-  height: 100vh;
   padding: 20px;
   text-align: center;
+}
 
+.weather-info {
+  display: flex;
+  flex-direction: column;
 }
 
 .day {
@@ -150,36 +304,24 @@ export default {
   background: linear-gradient(to right, #000000, #172429);
 }
 
-.weather-box {
-  font-size: 18px;
-  background: rgba(13, 14, 22, 0.2);
-  border: 2px solid rgb(228, 228, 228,0.3);
-  border-radius: 10px;
-  padding: 10px;
-  padding-top: 20px;
-  margin: 20px auto;
-  max-width: 350px;
-}
-
 .search-container {
-
-  background: rgba(209, 193, 248, 0.2);
   border-radius: 10px;
-  margin: 20px auto;
-  padding-bottom: 10px;
-  max-width: 320px;
+  margin: 30px auto;
+  width: 960px;
+  border: 1px solid rgba(255, 255, 255, 0.5);
 }
 
-.hourly-forecast,
-.daily-forecast {
-  margin-top: 40px;
-
+.current-weather-data {
+  font-size: 16px;
+  color: rgb(224, 232, 243);
+  text-align: left;
 }
 
-.forecast-grid {
-  display: flex;
-  justify-content: center;
-  gap: 10px;
+.weather-desc {
+  text-align: right;
+  margin-left: auto;
+  font-size: 16px;
+  color: rgb(224, 232, 243);
 }
 
 .hour-box,
@@ -195,6 +337,11 @@ export default {
   border: 1px solid rgba(255, 255, 255, 0.25);
   margin-left: 20px;
 }
+.day-box.selected {
+  background-color: rgba(234, 234, 234, 0.2);
+  border: 1px solid rgba(255, 255, 255, 0.5);
+}
+
 h2, h3 {
   margin: 20px 0;
   color: rgb(234, 224, 243);
@@ -213,13 +360,23 @@ hr {
   border: none;
   background-color: rgba(196, 212, 255, 0.35);
 }
-footer {
 
-  background: rgba(0, 0, 0, 0.2);
-  text-align: center;
-  padding: 20px 0px 5px 0px;
-  color: rgb(234, 224, 243);
-  bottom: 0;
-  width: 100%;
-  }
+.tabs {
+  margin: 0 5px;
+  color: rgb(224, 232, 243);
+  text-decoration: none !important;
+}
+
+.active {
+  text-decoration: underline !important;
+  text-decoration-color: rgb(224, 232, 243);
+  text-underline-offset: 6px;
+}
+
+.chart-container {
+  display: flex;
+  align-items: center;
+  margin: 0 auto;
+}
+
 </style>
